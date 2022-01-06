7ec50975e6048837de3f7cdc543b887c0ea3b2c4
igorocky <123@gmail.com>
2022-01-06 13:53:15 +0100

    Implement Repository.recreateTable().

--- app/src/main/java/org/igye/memoryrefresh/database/Repository.kt
+++ app/src/main/java/org/igye/memoryrefresh/database/Repository.kt
@@ -67,6 +67,150 @@ class Repository(
         """.trimIndent())
         tags.create(db)
         cardToTag.create(db)
+
+        recreateTable(
+            db = db,
+            tableName = cards.ver.tableName,
+            createTableBody = """ (
+                            ${cards.ver.verId} integer primary key autoincrement,
+                            ${cards.ver.timestamp} integer not null,
+                            ${cards.id} integer not null,
+                            ${cards.type} integer not null,
+                            ${cards.createdAt} integer not null
+                        ) """.trimIndent(),
+            oldColumnNames = listOf(
+                cards.ver.verId,
+                cards.ver.timestamp,
+                cards.id,
+                cards.type,
+                cards.createdAt,
+            )
+        )
+
+        recreateTable(
+            db = db,
+            tableName = cardsSchedule.ver.tableName,
+            createTableBody = """ (
+                            ${cardsSchedule.ver.verId} integer primary key autoincrement,
+                            ${cardsSchedule.ver.timestamp} integer not null,
+                            ${cardsSchedule.cardId} integer not null,
+                            ${cardsSchedule.updatedAt} integer not null,
+                            ${cardsSchedule.delay} text not null,
+                            ${cardsSchedule.randomFactor} real not null,
+                            ${cardsSchedule.nextAccessInMillis} integer not null,
+                            ${cardsSchedule.nextAccessAt} integer not null
+                        ) """.trimIndent(),
+            oldColumnNames = listOf(
+                cardsSchedule.ver.verId,
+                cardsSchedule.ver.timestamp,
+                cardsSchedule.cardId,
+                cardsSchedule.updatedAt,
+                cardsSchedule.delay,
+                cardsSchedule.randomFactor,
+                cardsSchedule.nextAccessInMillis,
+                cardsSchedule.nextAccessAt,
+            )
+        )
+
+        recreateTable(
+            db = db,
+            tableName = translationCards.ver.tableName,
+            createTableBody = """ (
+                    ${translationCards.ver.verId} integer primary key autoincrement,
+                    ${translationCards.ver.timestamp} integer not null,
+                    ${translationCards.cardId} integer not null,
+                    ${translationCards.textToTranslate} text not null,
+                    ${translationCards.translation} text not null
+                ) """.trimIndent(),
+            oldColumnNames = listOf(
+                translationCards.ver.verId,
+                translationCards.ver.timestamp,
+                translationCards.cardId,
+                translationCards.textToTranslate,
+                translationCards.translation,
+            )
+        )
+
+        recreateTable(
+            db = db,
+            tableName = translationCardsLog.tableName,
+            createTableBody = """ (
+                    ${translationCardsLog.recId} integer primary key autoincrement,
+                    ${translationCardsLog.timestamp} integer not null,
+                    ${translationCardsLog.cardId} integer not null,
+                    ${translationCardsLog.translation} text not null,
+                    ${translationCardsLog.matched} integer not null check (${translationCardsLog.matched} in (0,1))
+                ) """.trimIndent(),
+            oldColumnNames = listOf(
+                translationCardsLog.recId,
+                translationCardsLog.timestamp,
+                translationCardsLog.cardId,
+                translationCardsLog.translation,
+                translationCardsLog.matched,
+            ),
+            reconstructIndexes = listOf(
+                "CREATE INDEX IDX_${translationCardsLog}_CARD_ID on $this ( ${translationCardsLog.cardId}, ${translationCardsLog.timestamp} desc )"
+            )
+        )
+
+    }
+
+    /**
+     * https://www.sqlite.org/lang_altertable.html
+     *     -> 6. Making Other Kinds Of Table Schema Changes
+     */
+    private fun recreateTable(
+        db: SQLiteDatabase,
+        tableName: String,
+        createTableBody: String,
+        oldColumnNames: List<String>,
+        newColumnNames: List<String>? = null,
+        reconstructIndexes: List<String>? = null,
+    ) {
+        try {
+            //1. If foreign key constraints are enabled, disable them using PRAGMA foreign_keys=OFF.
+            db.execSQL("PRAGMA foreign_keys=OFF")
+            //2. Start a transaction.
+            db.doInTransaction {
+                //3. Remember the format of all indexes, triggers, and views associated with table X.
+                // This information will be needed in step 8 below. One way to do this is to run a query like
+                // the following: SELECT type, sql FROM sqlite_schema WHERE tbl_name='X'.
+                // NOTE: This step is not used in this implementation because of 'reconstructIndexes' parameter.
+
+                //4. Use CREATE TABLE to construct a new table "new_X" that is in the desired revised format of table X.
+                // Make sure that the name "new_X" does not collide with any existing table name, of course.
+                execSQL("CREATE TABLE new_$tableName $createTableBody")
+
+                //5. Transfer content from X into new_X using a statement like: INSERT INTO new_X SELECT ... FROM X.
+                execSQL("""
+                    INSERT INTO new_$tableName (${(newColumnNames?:oldColumnNames).joinToString(separator = ",")}) SELECT ${oldColumnNames.joinToString(separator = ",")} FROM $tableName
+                """.trimIndent())
+
+                //6. Drop the old table X: DROP TABLE X.
+                execSQL("DROP TABLE $tableName")
+
+                //7. Change the name of new_X to X using: ALTER TABLE new_X RENAME TO X.
+                execSQL("ALTER TABLE new_$tableName RENAME TO $tableName")
+
+                //8. Use CREATE INDEX, CREATE TRIGGER, and CREATE VIEW to reconstruct indexes, triggers, and views
+                // associated with table X. Perhaps use the old format of the triggers, indexes, and views saved from
+                // step 3 above as a guide, making changes as appropriate for the alteration.
+                reconstructIndexes?.forEach { execSQL(it) }
+
+                //9. If any views refer to table X in a way that is affected by the schema change, then drop those views
+                // using DROP VIEW and recreate them with whatever changes are necessary to accommodate the schema
+                // change using CREATE VIEW.
+
+                //10. If foreign key constraints were originally enabled then run PRAGMA foreign_key_check to verify
+                // that the schema change did not break any foreign key constraints.
+                execSQL("PRAGMA foreign_key_check")
+
+                //11. Commit the transaction started in step 2.
+            }
+        } finally {
+            //12. If foreign keys constraints were originally enabled, reenable them now.
+            db.execSQL("PRAGMA foreign_keys=ON")
+        }
     }
 }
 
